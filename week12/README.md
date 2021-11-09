c2db834b841c808314e88e773af5806724c9f918
Andrey Starodumov <penzaandron681@mail.ru>
2021-11-09 21:48:20 +0300

    Lab12 by Andrey Starodumov

---
+++ week12/README.md
@@ -0,0 +1,9 @@
+# LAB12 (Week12)
+
+I used wsl instead of virtual machine, so I could not to run my programs, and I hope they will work if you run them.
+They will create respective files: ex2.txt and ex3.txt
+
+### ex1:
+- because of wsl it is oly part of whole data from /proc/ioports
+### ex2 and ex3:
+- if you can, run it please
---
+++ week12/ex3.c
@@ -0,0 +1,49 @@
+#include <linux/input.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main() {
+
+    printf("ESC - is the key to exit and save output to ex3.txt\n");
+    printf("TRY: P+A+C; P+E, C+E\n");
+
+    FILE *out = fopen("ex3.txt", "w");
+
+    int *keysPressed = calloc(300, sizeof(int));
+
+    int keyboard = open("/dev/input/by-path/platform-i8042-serio-0-event-kbd", O_RDONLY);
+    struct input_event ievent;
+
+    
+    read(keyboard, &ievent, sizeof(ievent));
+    while (1) {
+        if(ievent.type == EV_KEY && ievent.value == 0) {
+            keysPressed[(int) ievent.code] = 0;
+            fprintf(out, "RELEASED 0x%04x (%i)\n", ievent.code, ievent.code);
+        }
+        if(ievent.type == EV_KEY && ievent.value == 1) {
+            keysPressed[(int) ievent.code] = 1;
+            fprintf(output, "PRESSED 0x%04x (%i)\n", ievent.code, ievent.code);
+        }
+        for (int i = 0; i < 300; i++) {
+            if (keysPressed[46] == 1 && keysPressed[30] == 1 && keysPressed[25] == 1) {
+                fprintf(out, "Get some cappuccino!\n");
+            }
+            if (keysPressed[25] == 1 && keysPressed[18] == 1) {
+                fprintf(out, "I passed the exam!\n");
+            }
+            if (keysPressed[18] == 1 && keysPressed[46] == 1) {
+                fprintf(out, "Great work!\n");
+            }
+        }
+        if (ievent.type == EV_KEY && ievent.code == KEY_ESC) {
+            break;
+        }
+    }
+    fclose(out);
+    close(keyboard);
+
+    return 0;
+}
